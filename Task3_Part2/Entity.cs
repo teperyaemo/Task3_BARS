a33d2e607be93be4f0bde32f7d0783add9defeac
teperyaemo <rokudenas@bk.ru>
2022-03-30 20:20:22 +0300

    +2 задача

---
+++ Task3_Part2/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3_Part2
+{
+    class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Task3_Part2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Entity> entities = new List<Entity>
+            {
+                new Entity() { Id = 1, ParentId = 0, Name = "Root entity" },
+                new Entity() { Id = 2, ParentId = 1, Name = "Child of 1 entity" },
+                new Entity() { Id = 3, ParentId = 1, Name = "Child of 1 entity" },
+                new Entity() { Id = 4, ParentId = 2, Name = "Child of 2 entity" },
+                new Entity() { Id = 5, ParentId = 4, Name = "Child of 4 entity" }
+            };
+
+            var FinalDictionary = SomeMet(entities);
+
+            foreach (var dict in FinalDictionary)
+            {
+                Console.WriteLine($"Key: {dict.Key}  Value count: {dict.Value.Count}");
+            }
+        }
+
+        static Dictionary<int, List<Entity>> SomeMet (List<Entity> list)
+        {
+            Dictionary<int, List<Entity>> dict = new Dictionary<int, List<Entity>>();
+
+            for (int key = 0; key < list.Count; key++)
+            {
+                var value = list.Where(x => x.ParentId == key).ToList();
+                if (value.Count() != 0) 
+                { 
+                    dict.TryAdd(key, value);
+                }
+            }
+
+            return dict;
+        }
+
+    }
+}
